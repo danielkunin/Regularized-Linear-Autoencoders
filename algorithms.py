00f402c864f9421e6025e8a79f3a218f17b79e20
bram <bgorissen@partners.org>
2019-02-11 10:42:36 -0500

    added iterative exact minimization method

--- algorithms.py
+++ algorithms.py
@@ -73,6 +73,30 @@ def LAE_PCA_Oja_GD(W2):
     t = end - start
     return (u,s,t,i)
 
+# iterative exact minimization
+def LAE_PCA_exact(W1, W2, u_svd = None):
+    W1 = W1.copy()
+    W2 = W2.copy()
+    start = time.time()
+    XXt = X @ X.T
+    i = 0
+    while np.linalg.norm(W1 - W2.T) > eps:
+        coefficient_matrix = np.kron(W2.T @ W2,XXt) # order reversed since matrices are stored per row
+        np.fill_diagonal(coefficient_matrix, coefficient_matrix.diagonal() + lamb)
+        W1 = scipy.linalg.solve(coefficient_matrix, (W2.T @ XXt).reshape((m*k,1)), assume_a = 'pos').reshape((k,m))
+        
+        right_hand_side = W1 @ XXt
+        coefficient_matrix = right_hand_side @ W1.T
+        np.fill_diagonal(coefficient_matrix, coefficient_matrix.diagonal() + lamb)
+        W2 = scipy.linalg.solve(coefficient_matrix, right_hand_side, assume_a = 'pos').T
+        
+        i += 1
+    
+    u, s, _ = np.linalg.svd(W2, full_matrices = False)
+    s = np.sqrt(lamb / (1 - s**2))
+    end = time.time()
+    t = end - start
+    return (u,s,t,i)
 
 def display(method, t, i, u, s):
     if i == None:
@@ -94,3 +118,6 @@ display('LAE-PCA (GD)', t, i2, u, s)
 
 (u,s,t,i3) = LAE_PCA_Oja_GD(W2)
 display('Regularized Oja\'s rule (GD)', t, i3, u, s)
+
+(u,s,t,i4) = LAE_PCA_exact(W1, W2)
+display('LAE-PCA (Exact)', t, i4, u, s)
