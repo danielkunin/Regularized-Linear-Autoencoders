86a4b16317622484f02b42e5bb4955258c4027e1
Daniel Kunin <daniel.kunin@gmail.com>
2019-02-11 18:30:26 -0800

    add plots per time

--- algorithms.py
+++ algorithms.py
@@ -47,33 +47,38 @@ def randomized_svd_wrapper():
 def LAE_PCA_untied(W1, W2, u_svd = None):
     W1 = W1.copy()
     W2 = W2.copy()
+    dist = []
+    times = []
     start = time.time()
     XXt = X @ X.T
     i = 0
-    dist = []
     while np.linalg.norm(W1 - W2.T) > eps:
         W1 -= alpha * ((W2.T @ (W2 @ W1 - I)) @ XXt + lamb * W1)
         W2 -= alpha * (((W2 @ W1 - I) @ XXt) @ W1.T + lamb * W2)
         
         if u_svd is not None:
+            end = time.time()
             dist.append( compute_svd_error(W2, k, u_svd) )
+            times.append(end - start)
+            start = time.time()
         i += 1
         
     u, s, _ = np.linalg.svd(W2, full_matrices = False)
     s = np.sqrt(lamb / (1 - s**2))
     end = time.time()
     t = end - start
-    return (u, s, t, i, dist)
+    return (u, s, t, i, dist, times)
 
 # LAE-PCA using tied weights and gradient descent
 def LAE_PCA_tied(W2, u_svd = None):
     W1 = W2.copy().T
     W2 = W2.copy()
+    dist = []
+    times = []
     start = time.time()
     XXt = X @ X.T
     diff = np.inf
     i = 0
-    dist = []
     while diff > eps:
         W1_update = alpha * ((W2.T @ (W2 @ W1 - I)) @ XXt + lamb * W1)
         W2_update = alpha * (((W2 @ W1 - I) @ XXt) @ W1.T + lamb * W2)
@@ -82,22 +87,26 @@ def LAE_PCA_tied(W2, u_svd = None):
         diff = np.linalg.norm(W1_update) + np.linalg.norm(W2_update)
         
         if u_svd is not None:
+            end = time.time()
             dist.append( compute_svd_error(W2, k, u_svd) )
+            times.append(end - start)
+            start = time.time()
         i += 1
         
     u, s, _ = np.linalg.svd(W2, full_matrices = False)
     s = np.sqrt(lamb / (1 - s**2))
     end = time.time()
     t = end - start
-    return (u, s, t, i, dist)
+    return (u, s, t, i, dist, times)
 
 # LAE-PCA using single weight matrix and gradient descent (Regularized Oja's Rule)
 def LAE_PCA_oja(W2, u_svd = None):
     W2 = W2.copy()
+    dist = []
+    times = []
     start = time.time()
     XXt = X @ X.T
     diff = np.inf
-    dist = []
     i = 0
     while diff > eps:
         update = alpha * (((W2 @ W2.T - I) @ XXt) @ W2 + lamb * W2)
@@ -105,23 +114,27 @@ def LAE_PCA_oja(W2, u_svd = None):
         diff = np.linalg.norm(update)
         
         if u_svd is not None:
+            end = time.time()
             dist.append( compute_svd_error(W2, k, u_svd) )
+            times.append(end - start)
+            start = time.time()
         i += 1
         
     u, s, _ = np.linalg.svd(W2, full_matrices = False)
     s = np.sqrt(lamb / (1 - s**2))
     end = time.time()
     t = end - start
-    return (u, s, t, i, dist)
+    return (u, s, t, i, dist, times)
 
 # LAE-PCA using untied weight matrices and alternating Newton's method
 def LAE_PCA_exact(W1, W2, u_svd = None):
     W1 = W1.copy()
     W2 = W2.copy()
+    dist = []
+    times = []
     start = time.time()
     XXt = X @ X.T
     i = 0
-    dist = []
     while np.linalg.norm(W1 - W2.T) > eps:
         coefficient_matrix = np.kron(W2.T @ W2, XXt) # order reversed since matrices are stored per row
         np.fill_diagonal(coefficient_matrix, coefficient_matrix.diagonal() + lamb)
@@ -133,14 +146,17 @@ def LAE_PCA_exact(W1, W2, u_svd = None):
         W2 = scipy.linalg.solve(coefficient_matrix, right_hand_side, assume_a = 'pos').T
         
         if u_svd is not None:
+            end = time.time()
             dist.append( compute_svd_error(W2, k, u_svd) )
+            times.append(end - start)
+            start = time.time()
         i += 1
     
     u, s, _ = np.linalg.svd(W2, full_matrices = False)
     s = np.sqrt(lamb / (1 - s**2))
     end = time.time()
     t = end - start
-    return (u, s, t, i, dist)
+    return (u, s, t, i, dist, times)
 
 
 def display(method, t, i, u, s):
@@ -159,23 +175,23 @@ display('SVD', t, None, u_svd, s)
 (u, s, t) = randomized_svd_wrapper()
 display('Randomized SVD', t, None, u, s)
 
-(u, s, t, i, _) = LAE_PCA_untied(W1, W2, None)
+(u, s, t, i, _, _) = LAE_PCA_untied(W1, W2, None)
 display('LAE-PCA (untied)', t, i, u, s)
 
-(u, s, t, i, _) = LAE_PCA_tied(W2, None)
+(u, s, t, i, _, _) = LAE_PCA_tied(W2, None)
 display('LAE-PCA (tied)', t, i, u, s)
 
-(u, s, t, i, _) = LAE_PCA_oja(W2, None)
+(u, s, t, i, _, _) = LAE_PCA_oja(W2, None)
 display('LAE-PCA (oja)', t, i, u, s)
 
-(u, s, t, i, _) = LAE_PCA_exact(W1, W2, None)
+(u, s, t, i, _, _) = LAE_PCA_exact(W1, W2, None)
 display('LAE-PCA (exact)', t, i, u, s)
 
 # perform diagnostic runs
-(_ ,_ ,_ ,_ ,dist2) = LAE_PCA_untied(W1, W2, u_svd)
-(_ ,_ ,_ ,_ ,dist3) = LAE_PCA_tied(W2, u_svd)
-(_ ,_ ,_ ,_ ,dist4) = LAE_PCA_oja(W2, u_svd)
-(_ ,_ ,_ ,_ ,dist5) = LAE_PCA_exact(W1, W2, u_svd)
+(_ ,_ ,_ ,_ ,dist2,times2) = LAE_PCA_untied(W1, W2, u_svd)
+(_ ,_ ,_ ,_ ,dist3,times3) = LAE_PCA_tied(W2, u_svd)
+(_ ,_ ,_ ,_ ,dist4,times4) = LAE_PCA_oja(W2, u_svd)
+(_ ,_ ,_ ,_ ,dist5,times5) = LAE_PCA_exact(W1, W2, u_svd)
 
 plt.plot(dist2)
 plt.plot(dist3)
@@ -184,5 +200,15 @@ plt.plot(dist5)
 plt.legend(['LAE-PCA (untied)', 'LAE-PCA (tied)', 'LAE-PCA (oja)', 'LAE-PCA (exact)'])
 plt.title('Rate of convergence')
 plt.xlabel('Iteration')
+plt.ylabel('Error in SVD factor U')
+plt.show()
+
+plt.plot(np.cumsum(times2), dist2)
+plt.plot(np.cumsum(times3), dist3)
+plt.plot(np.cumsum(times4), dist4)
+plt.plot(np.cumsum(times5), dist5)
+plt.legend(['LAE-PCA (untied)', 'LAE-PCA (tied)', 'LAE-PCA (oja)', 'LAE-PCA (exact)'])
+plt.title('Rate of convergence')
+plt.xlabel('Time (sec)')
 plt.ylabel('Error in SVD factor U')
 plt.show()
